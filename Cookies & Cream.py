9d1cc63caba4044de2865dd582b4e0e22799933d
Kirsi <mushi88@users.noreply.github.com>
2017-05-22 11:31:59 +1000

    Update Cookies & Cream.py

--- Cookies & Cream.py
+++ Cookies & Cream.py
@@ -6,3 +6,10 @@ class Window(Frame):
     def __init__(self, master = None)
         Frame.__init__(self, master)
         self.master = master
+        self.LoadUI()
+    
+    def LoadUI(self):
+        self.MenuBar = Menu(self)
+        self.Connect = Menu(self.MenuBar, tearoff = 0)
+        
+        self.MenuBar.add_cascade(label = "Lan", menu = self.Connect)

564c0d21c0c9eaea9bf4c8e8adac6e6857bbd989
Inga Babe <anyipos@hotmail.com>
2017-05-25 09:43:25 +1000

    Update CnC.java

--- CnC.java
+++ CnC.java
@@ -1,8 +1,14 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import javax.swing.JOptionPane;
 
 public class CnC {
-    public static void main(Stirng[] args) {
-  
-    }
+    public static void main(String[] args) throws Exception {
+        Server myServer = new Server();
+        Client myClient = new Client();
+		myServer.start();
+		JOptionPane.showMessageDialog(null,"Click ok to start the server" );
+		myClient.start();
+    }  
 }
